e468792bea98f96abcdac0e2173b1a45d341c386
zhware <zh@zhware.net>
2025-09-08 21:50:37 +0900

    Remove duplicate files

--- hooks/index.js
+++
@@ -1,4 +0,0 @@
-export { default as useConnectWallet } from './useConnectWallet';
-export { default as useBalance } from './useBalance';
-export { default as useXecPrice } from './useXecPrice';
-export { default as useTokens } from './useTokens';
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
-export { default as useConnectWallet } from './useConnectWallet';
 export { default as useBalance } from './useBalance';
+export { default as useConnectWallet } from './useConnectWallet';
+export { useTranslation } from './useTranslation';
 export { useToken } from './useToken';
